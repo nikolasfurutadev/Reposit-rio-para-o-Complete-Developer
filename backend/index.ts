cba79675b7c2cd604feb04f40b2802e5da46153d
Nikolas <nikolasfuruta.dev@gmail.com>
2025-09-05 14:06:39 -0300

    cap4 finished

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,8 @@ import { routeHello, routeAPINames, routeWeather } from './routes.js';
 
 import express, { Request, Response } from 'express';
 
+import path  from 'path';
+
 const server = express();
 const port = 3000;
 
@@ -26,8 +28,14 @@ server.get("/api/weather/:zipcode",
     (req: Request, res: Response): void => {
     const response = routeWeather({zipcode: req.params.zipcode});
     res.send(response);
-    });
-    
+});
+
+server.get("/components/weather", (req: Request, res: Response): void => {
+    const filePath = path.join(process.cwd(), 'public', 'weather.html');
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
+});
+
 server.listen(port, () => {
-    console.log("Listening on port " + port);
+    console.log(`Server is running at http://localhost:${port}`);
 });
