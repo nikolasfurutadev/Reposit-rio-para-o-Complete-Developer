d40fce45d9ac3f3890cc8775cff4d3677104d9d8
Nikolas <nikolasfuruta.dev@gmail.com>
2025-09-11 17:22:29 -0300

    create ssr file

---
+++ my-app/pages/names-ssr.tsx
@@ -0,0 +1,38 @@
+import type {
+    GetServerSideProps,
+    GetServerSidePropsContext,
+    InferGetServerSidePropsType,
+    NextPage,
+    PreviewData
+} from "next";
+import { ParsedUrlQuery } from "querystring";
+import { fetchNames } from "../util/fetch-names";
+
+type responseItemType = {
+    id: string;
+    name: string;
+};
+
+const NamesSSR: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    const output = props.names.map((item: responseItemType, idx: number)=>{
+        return(
+            <li key={`name-${idx}`}>{item.id}: {item.name}</li>
+        );
+    });
+    return (
+        <ul>{output}</ul>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+    context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+) => {
+    let names: responseItemType[]|[] = [];
+    try {
+        names = await fetchNames();
+    } catch (error) {}
+    return {
+            props: {names}
+    };
+};
+export default NamesSSR;
